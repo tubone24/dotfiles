63ac995767652d5df25b2f54f3b26f23f72feb4e
tubone <tubo.yyyuuu@gmail.com>
2021-06-19 12:44:07 +0900

    installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cp -r .userscript ~/
+cp -r .vim ~/
+cp .agignore ~/
+cp .ansible.cfg ~/
+cp .gitconfig ~/
+cp .tmux.conf ~/
+cp .vimrc ~/
+cp .zprofile ~/
+cp .zshrc ~/
+mkdir ~/.cache
+source ~/.zshrc
