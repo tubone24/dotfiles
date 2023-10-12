2c06f0a58575fa845459205e408ce890aaece5b6
tubone(Yu Otsubo) <yu.otsubo@eventhub.jp>
2023-10-12 15:46:32 +0900

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -21,6 +21,9 @@ end
 if wezterm.target_triple == "x86_64-apple-darwin" then 
   default_prog = { 'zsh', '-l' }
 end
+if wezterm.target_triple == "aarch64-apple-darwin" then 
+  default_prog = { 'zsh', '-l' }
+end
 
 -- Equivalent to POSIX basename(3)
 -- Given "/foo/bar" returns "bar"
@@ -63,87 +66,111 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 	}
 end)
 
--- 右ステータスのカスタマイズ
-wezterm.on("update-right-status", function(window, pane)
-    local cells = {};
-    -- 現在のディレクトリ
-    local cwd_uri = pane:get_current_working_dir()
-    if cwd_uri then
-      cwd_uri = cwd_uri:sub(8);
-      local slash = cwd_uri:find("/")
-      local cwd = ""
-      local hostname = ""
-      local leader = ''
-      if window:leader_is_active() then
-        leader = 'LEADER'
-      end
-      -- paneの累計IDを取得
-      local pane_id = pane:pane_id()
-      if slash then
-        hostname = cwd_uri:sub(1, slash-1)
-        local dot = hostname:find("[.]")
-        if dot then
-          hostname = hostname:sub(1, dot-1)
-        end
-        cwd = cwd_uri:sub(slash)
-  
-        table.insert(cells, cwd);
-        table.insert(cells, pane_id);
-        table.insert(cells, leader);
-      end
-    end
-  
-    -- 時刻表示
-    local date = wezterm.strftime("%m/%d %H:%M:%S %a");
-    table.insert(cells, wezterm.nerdfonts.mdi_clock .. '  ' .. date);
-  
-    -- バッテリー
-    for _, b in ipairs(wezterm.battery_info()) do
-      table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
-    end
-  
-    -- The powerline < symbol
-    local LEFT_ARROW = utf8.char(0xe0b3);
-    -- The filled in variant of the < symbol
-    local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-  
-    -- Color palette for the backgrounds of each cell
-    local colors = {
-      "#3c1361",
-      "#52307c",
-      "#663a82",
-      "#7c5295",
-      "#b491c8",
-    };
-  
-    -- Foreground color for the text across the fade
-    local text_fg = "#c0c0c0";
-  
-    -- The elements to be formatted
-    local elements = {};
-    -- How many cells have been formatted
-    local num_cells = 0;
-  
-    -- Translate a cell into elements
-    function push(text, is_last)
-      local cell_no = num_cells + 1
-      table.insert(elements, {Foreground={Color=text_fg}})
-      table.insert(elements, {Background={Color=colors[cell_no]}})
-      table.insert(elements, {Text=" "..text.." "})
-      if not is_last then
-        table.insert(elements, {Foreground={Color=colors[cell_no+1]}})
-        table.insert(elements, {Text=SOLID_LEFT_ARROW})
-      end
-      num_cells = num_cells + 1
-    end
-  
-    while #cells > 0 do
-      local cell = table.remove(cells, 1)
-      push(cell, #cells == 0)
-    end
-  
-    window:set_right_status(wezterm.format(elements));
-  end);
+-- 左
+
+local DEFAULT_FG = { Color = '#9a9eab' }
+local DEFAULT_BG = { Color = '#333333' }
+
+local SPACE_1 = ' '
+local SPACE_3 = '   '
+
+local HEADER_KEY_NORMAL = { Foreground = DEFAULT_FG, Text = '' }
+local HEADER_LEADER = { Foreground = { Color = '#ffffff' }, Text = '' }
+local HEADER_IME = { Foreground = DEFAULT_FG, Text = 'あ' }
+
+local HEADER_HOST = { Foreground = { Color = '#75b1a9' }, Text = '' }
+local HEADER_CWD = { Foreground = { Color = '#92aac7' }, Text = '' }
+local HEADER_DATE = { Foreground = { Color = '#ffccac' }, Text = '󱪺' }
+local HEADER_TIME = { Foreground = { Color = '#bcbabe' }, Text = '' }
+local HEADER_BATTERY = { Foreground = { Color = '#dfe166' }, Text = '' }
+
+local function AddElement(elems, header, str)
+  table.insert(elems, { Foreground = header.Foreground })
+  table.insert(elems, { Background = DEFAULT_BG })
+  table.insert(elems, { Text = header.Text .. SPACE_1 })
+
+  table.insert(elems, { Foreground = DEFAULT_FG })
+  table.insert(elems, { Background = DEFAULT_BG })
+  table.insert(elems, { Text = str .. SPACE_3 })
+end
+
+local function AddIcon(elems, icon)
+  table.insert(elems, { Foreground = icon.Foreground })
+  table.insert(elems, { Background = DEFAULT_BG })
+  table.insert(elems, { Text = SPACE_1 .. icon.Text .. SPACE_3 })
+end
+
+local function GetKeyboard(elems, window)
+  if window:leader_is_active() then
+    AddIcon(elems, HEADER_LEADER)
+    return
+  end
+
+  AddIcon(elems, window:composition_status() and HEADER_IME or HEADER_KEY_NORMAL)
+end
+
+local function LeftUpdate(window, pane)
+  local elems = {}
+
+  GetKeyboard(elems, window)
+
+  window:set_left_status(wezterm.format(elems))
+end
+
+local function GetHostAndCwd(elems, pane)
+  local uri = pane:get_current_working_dir()
+
+  if not uri then
+    return
+  end
+
+  local cwd_uri = uri:sub(8)
+  local slash = cwd_uri:find '/'
+
+  if not slash then
+    return
+  end
+
+  local host = cwd_uri:sub(1, slash - 1)
+  local dot = host:find '[.]'
+
+  AddElement(elems, HEADER_HOST, dot and host:sub(1, dot - 1) or host)
+  AddElement(elems, HEADER_CWD, cwd_uri:sub(slash))
+end
+
+local function GetDate(elems)
+  AddElement(elems, HEADER_DATE, wezterm.strftime '%a %b %-d')
+end
+
+local function GetTime(elems)
+  AddElement(elems, HEADER_TIME, wezterm.strftime '%H:%M')
+end
+
+local function GetBattery(elems, window)
+  if not window:get_dimensions().is_full_screen then
+    return
+  end
+
+  for _, b in ipairs(wezterm.battery_info()) do
+    AddElement(elems, HEADER_BATTERY, string.format('%.0f%%', b.state_of_charge * 100))
+  end
+end
+
+local function RightUpdate(window, pane)
+  local elems = {}
+
+  GetHostAndCwd(elems, pane)
+  GetDate(elems)
+  GetBattery(elems, window)
+  GetTime(elems)
+
+  window:set_right_status(wezterm.format(elems))
+end
+
+wezterm.on('update-status', function(window, pane)
+  LeftUpdate(window, pane)
+  RightUpdate(window, pane)
+end)
 
 return {
     -- https://wezfurlong.org/wezterm/colorschemes/v/index.html?highlight=VibrantInk#vibrantink
@@ -157,6 +184,7 @@ return {
     enable_scroll_bar = true,
     default_prog = default_prog,
     ssh_domains = ssh_domains,
+    status_update_interval = 1000,
     visual_bell = {
         fade_in_function = 'EaseIn',
         fade_in_duration_ms = 105,
